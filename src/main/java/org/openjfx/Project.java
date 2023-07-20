fba990e5613e17971e533c5fc3fc0a43ff93190d
latxh <latxhman@gmail.com>
2023-07-20 14:06:52 -0400

    Projects page and project model

---
+++ src/main/java/org/openjfx/Project.java
@@ -0,0 +1,22 @@
+package org.openjfx;
+
+public class Project {
+    private String name;
+    private String date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
