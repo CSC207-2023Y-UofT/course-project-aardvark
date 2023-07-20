bb7bd5b6b0d525499a4c1eb2da5947db2aa29e12
latxh <latxhman@gmail.com>
2023-07-20 14:06:52 -0400

    Projects page and project model

--- src/main/java/org/openjfx/FXMLController.java
+++ src/main/java/org/openjfx/FXMLController.java
@@ -1,20 +1,65 @@
 package org.openjfx;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class FXMLController {
+public class FXMLController implements Initializable {
+
+    @FXML
+    private VBox projectsLayout;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        List<Project> projects = new ArrayList<>(projects());
+        for (int i=0; i<projects.size(); i++) {
+            FXMLLoader fxmlloader = new FXMLLoader();
+            fxmlloader.setLocation(getClass().getResource("project_item.fxml"));
+
+            try {
+                HBox hbox = fxmlloader.load();
+                ProjectItemController pic = fxmlloader.getController();
+                pic.setData(projects.get(i));
+                projectsLayout.getChildren().add(hbox);
+            } catch (IOException e) {
+                System.out.println("Something went wrong, FXML Load");
+            } catch (NullPointerException e) {
+                System.out.println("Something went wrong, FXML Load");
+            }
+        }
+    }
+
+    private  List<Project> projects() {
+//      Temporary code to simulate project list
+        List<Project> ls = new ArrayList<Project>();
+
+        for (int i = 0; i < 15; i++) {
+            Project project = new Project();
+            project.setName("Untitled Project");
+            project.setDate("Modified July 20");
+            ls.add(project);
+        }
+
+        return ls;
+    };
+
+
     private Stage stage;
     private Scene scene;
     private Parent root;
 
+    @FXML
     public void switchToSignUp(javafx.event.ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("signup.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -23,7 +68,7 @@ public class FXMLController {
         stage.setScene(scene);
         stage.show();
     }
-
+    @FXML
     public void switchToSignIn(javafx.event.ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("signin.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -32,5 +77,13 @@ public class FXMLController {
         stage.setScene(scene);
         stage.show();
     }
-
+    @FXML
+    public void switchToProjects(javafx.event.ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("projects.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/org/openjfx/Project.java
+++ src/main/java/org/openjfx/Project.java
@@ -1,51 +1,22 @@
 package org.openjfx;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Stack;
-
 public class Project {
-    private String projectName;
-    private List<VisualElement> elements;
-    private Stack<VisualElement> redoStack;
+    private String name;
+    private String date;
 
-    public Project(String name) {
-        projectName = name;
-        elements = new ArrayList<>();
-        redoStack = new Stack<>();
+    public String getName() {
+        return name;
     }
 
-    public void setProjectName(String name) {
-        projectName = name;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getProjectName() {
-        return projectName;
+    public String getDate() {
+        return date;
     }
 
-    public void addVisualElement(VisualElement v) {
-        elements.add(v);
-    }
-
-    public void undoVisualElement() {
-        VisualElement v = elements.remove(elements.size() - 1);
-        redoStack.add(v);
-    }
-
-    /**
-     * If redoStack is not empty, adds the last undone
-     * visualElement back to the element list.
-     * Otherwise, does nothing.
-     */
-    public void redoVisualElement() {
-        if( redoStack.empty() ) {
-            return;
-        }
-        VisualElement v = redoStack.pop();
-        elements.add(v);
-    }
-
-    public boolean canRedo() {
-        return !redoStack.empty();
+    public void setDate(String date) {
+        this.date = date;
     }
 }
