859c73c0e159dfffdd4a53edc721693cc3c45274
AlexMirabella <alexander.mirabella@mail.utoronto.ca>
2023-07-20 19:05:53 -0400

    Removed unused date import

--- src/main/java/org/openjfx/FXMLController.java
+++ src/main/java/org/openjfx/FXMLController.java
@@ -14,7 +14,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.Date;
 
 public class FXMLController implements Initializable {
 
