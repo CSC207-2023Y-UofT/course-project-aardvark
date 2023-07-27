da84636e6f0106a26378ce245b58b4395648fc90
je3songli <58784080+je3songli@users.noreply.github.com>
2023-07-27 19:17:05 -0400

    Added text features, connected to FXMLController

--- src/main/java/text/AardText.java
+++ src/main/java/text/AardText.java
@@ -2,29 +2,41 @@ package text;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.text.Font;
+import javafx.scene.paint.Color;
 import org.openjfx.VisualElement;
 
 public class AardText implements VisualElement {
-    private String text;
-    private String fontFamily;
-    private double fontSize;
-    // private String colour;
+    private final String text;
+    private final String fontFamily;
+    private final double fontSize;
+    private final Color color;
 
-    private double[] coordinates;
+    private final double[] coordinates;
 
-    public AardText(String text, Font font, double x, double y){
+    public AardText(String text, Color color, Font font, double x, double y){
+        this.text = text;
         this.fontFamily = font.getFamily();
         this.fontSize = font.getSize();
-        this.text = text;
+        this.color = color;
         coordinates = new double[2];
         coordinates[0] = x;
         coordinates[1] = y;
     }
 
+    public String getStringCoordinates(){
+        return "(" + coordinates[0] + ", " + coordinates[1] + ")";
+    }
+
     @Override
     public void draw(GraphicsContext gc) {
         gc.setFont(Font.font(fontFamily, fontSize));
+        gc.setFill(color);
         gc.fillText(text, coordinates[0], coordinates[1]);
+    }
+
+    @Override
+    public String toString(){
+        return this.text + ", " + getStringCoordinates() + ", " + this.fontFamily + ", " + this.fontSize + ", " + this.color;
     }
 
 }
--- src/main/java/text/TextMain.java
+++ src/main/java/text/TextMain.java
@@ -1,6 +1,10 @@
 package text;
 
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -9,42 +13,49 @@ import javafx.geometry.Orientation;
 import javafx.geometry.VPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Separator;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.util.StringConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.UnaryOperator;
 
 public class TextMain extends Application{
 
-
     public void start(Stage stage) throws Exception {
 
+        ArrayList<AardText> textArrayList = new ArrayList<AardText>();
         stage.setTitle("Writing Text");
+        stage.setResizable(false);
 
         HBox root = new HBox();
         VBox toolsRoot = new VBox();
+        VBox canvasRoot = new VBox();
         Separator HBoxSeparator = new Separator();
         HBoxSeparator.setMaxWidth(10);
         HBoxSeparator.setOrientation(Orientation.VERTICAL);
 
         Canvas textCanvas = new Canvas(300, 300);
         GraphicsContext gc = textCanvas.getGraphicsContext2D();
-        /* This is the default font and text that will be used */
+
         Font defaultFont = Font.font("Verdana", 15);
         gc.setFont(defaultFont);
         String [] defaultInput = {"ant"};
+        IntegerProperty sizeLabelProperty = new SimpleIntegerProperty(15);
 
         /*===== Creating Text =====*/
 
         /* Setting the Text */
-
         TextField textField = new TextField("ant");
         EventHandler<ActionEvent> setTextHandler = new EventHandler<ActionEvent>() {
             @Override
@@ -53,7 +64,23 @@ public class TextMain extends Application{
             }
         };
         textField.setOnAction(setTextHandler);
-        toolsRoot.getChildren().add(textField);
+
+        TextInputDialog textDialog = new TextInputDialog();
+        textDialog.setTitle("Input Text");
+        // TextField dialogTextField = new TextField();
+        textDialog.setHeaderText("Please write");
+
+        textDialog.setX(10);
+        textDialog.setY(10);
+
+        EventHandler<DialogEvent> getTextHandler = new EventHandler<DialogEvent>() {
+            @Override
+            public void handle(DialogEvent event) {
+                defaultInput[0] = textDialog.getResult();
+                System.out.println(defaultInput[0]);
+            }
+        };
+        textDialog.setOnCloseRequest(getTextHandler);
 
         /* Creating Text at a Specified Location */
         EventHandler<MouseEvent> writeTextHandler = new EventHandler<MouseEvent>() {
@@ -61,13 +88,61 @@ public class TextMain extends Application{
             public void handle(MouseEvent event) {
                 double xValue = event.getX();
                 double yValue = event.getY();
-                AardText newText = new AardText(defaultInput[0], gc.getFont(), xValue, yValue);
+                textDialog.showAndWait();
+                AardText newText = new AardText(defaultInput[0], (Color) gc.getFill(), gc.getFont(), xValue, yValue);
+                textArrayList.add(newText);
                 newText.draw(gc);
+                printTextArray(textArrayList);
             }
         };
         textCanvas.addEventFilter(MouseEvent.MOUSE_CLICKED, writeTextHandler);
 
+
+        canvasRoot.getChildren().add(textCanvas);
+        canvasRoot.getChildren().add(textField);
         /*===== Changing and Adjusting Text-Related Settings on the Canvas =====*/
+
+        /* Changing Font Family */
+        System.out.println(Font.getFamilies());
+        final ComboBox<String> fontComboBox = new ComboBox<String>();
+//        for (String family: Font.getFamilies()){
+//            fontComboBox.getItems().add(family);
+//        }
+        fontComboBox.getItems().addAll(
+                "Algerian",
+                "Arial",
+                "Cooper Black",
+                "Century Gothic",
+                "Verdana"
+        );
+        fontComboBox.setValue("Verdana");
+        EventHandler<ActionEvent> changeFontHandler = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Font newFont = new Font(fontComboBox.getValue(), sizeLabelProperty.get());
+                gc.setFont(newFont);
+            }
+        };
+        fontComboBox.addEventFilter(ActionEvent.ACTION, changeFontHandler);
+        toolsRoot.getChildren().add(fontComboBox);
+
+
+        /* Changing Text Colour */
+        ColorPicker colorPicker = new ColorPicker();
+        colorPicker.setValue(Color.BLACK);
+        EventHandler<ActionEvent> changeColorHandler = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                gc.setFill(colorPicker.getValue());
+            }
+        };
+        colorPicker.addEventFilter(ActionEvent.ACTION, changeColorHandler);
+        toolsRoot.getChildren().add(colorPicker);
+
+        /* Setting Text Size */
+        Label sizeLabel = new Label("15");
+        sizeLabel.textProperty().bind(sizeLabelProperty.asString("Font size: %d"));
+        toolsRoot.getChildren().add(sizeLabel);
 
         /* Increasing Text Size */
         Button btIncreaseText = new Button("Text++");
@@ -76,19 +151,41 @@ public class TextMain extends Application{
             public void handle(MouseEvent event) {
                 Font currentFont = gc.getFont();
                 Font newFont = Font.font(currentFont.getFamily(), currentFont.getSize() + 1);
+                sizeLabelProperty.set(sizeLabelProperty.get() + 1);
                 gc.setFont(newFont);
             }
         };
         btIncreaseText.addEventFilter(MouseEvent.MOUSE_CLICKED, increaseFontHandler);
-
         toolsRoot.getChildren().add(btIncreaseText);
+
+        /* Decreasing Text Size */
+        Button btDecreaseText = new Button("Text--");
+        EventHandler<MouseEvent> decreaseFontHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Font currentFont = gc.getFont();
+                Font newFont = Font.font(currentFont.getFamily(), currentFont.getSize() - 1);
+                sizeLabelProperty.set(sizeLabelProperty.get() - 1);
+                gc.setFont(newFont);
+            }
+        };
+        btDecreaseText.addEventFilter(MouseEvent.MOUSE_CLICKED, decreaseFontHandler);
+        toolsRoot.getChildren().add(btDecreaseText);
+
         root.getChildren().add(toolsRoot);
         root.getChildren().add(HBoxSeparator);
-        root.getChildren().add(textCanvas);
+        root.getChildren().add(canvasRoot);
         Scene sc = new Scene(root);
         stage.setScene(sc);
         stage.show();
     }
+
+    public void printTextArray(ArrayList<AardText> textArrayList){
+        for (AardText item: textArrayList){
+            System.out.println(item);
+        }
+    }
+
 }
 
 
