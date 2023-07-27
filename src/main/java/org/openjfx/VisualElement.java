da3dcda718440223865b9c60eaa2b3f0d38ddb7b
je3songli <58784080+je3songli@users.noreply.github.com>
2023-07-27 19:28:39 -0400

    VisualElement revert

--- src/main/java/org/openjfx/VisualElement.java
+++ src/main/java/org/openjfx/VisualElement.java
@@ -1,4 +1,16 @@
 package org.openjfx;
 
+import javafx.scene.canvas.GraphicsContext;
+
+import java.util.ArrayList;
+
 public interface VisualElement {
+    void draw(GraphicsContext gc);
 }
+
+// Line
+
+// Circle
+
+// Rectangle
+
