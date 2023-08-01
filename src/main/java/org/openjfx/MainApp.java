349f9b76fe5862f96c92c036787a590014e0643d
SquaredPiano <vishnukishan123@gmail.com>
2023-07-31 22:44:59 -0400

    Integrated the entire UI with the EditorController (all UI elements now work, namely Free Draw, Resize Canvas, Export as PNG, Clear All, the Text Box feature, Color Picking, the Brush Size feature, the Font Size feature, Font Size Increase and Decrease Buttons, and the Eraser)

--- src/main/java/org/openjfx/MainApp.java
+++ src/main/java/org/openjfx/MainApp.java
@@ -32,5 +32,22 @@ public class MainApp extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(convert(i));
+        }
+    }
+
+    public static String convert(int decide) {
+        if (decide % 15 == 0) {
+            return "tutorial.HelloWorld";
+        }
+        if (decide % 3 == 0) {
+            return "Hello";
+        }
+        if (decide % 5 == 0) {
+            return "World";
+        }
+        return String.valueOf(decide);
     }
 }
