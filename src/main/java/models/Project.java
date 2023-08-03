8799d6508761d07920f846f3beab919f8f0b4ae3
AlexMirabella <alexander.mirabella@mail.utoronto.ca>
2023-08-03 11:00:30 -0400

    branch after Brain's UI rehaul

--- src/main/java/models/Project.java
+++ src/main/java/models/Project.java
@@ -52,7 +52,7 @@ public class Project {
     }
 
     public AardCircle getLastAndRemoveCircle() {
-        AardCircle tmp = new AardCircle(0, 0, 0, true, true, Color.AQUA, Color.AQUA, 0);
+        AardCircle tmp = null;
         for (int i = elements.size() - 1; i >= 0; --i) {
             if (elements.get(i) instanceof AardCircle) {
                 AardCircle t = (AardCircle)elements.get(i);
